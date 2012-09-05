16974a2ba7020b5959afa8e19b242123f3e94a05
Atul Varma <varmaa@gmail.com>
2012-09-05 13:20:59 -0400

    removed config.sample.js

--- config.sample.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  s3: {
-    key: 'your amazon s3 key',
-    secret: 'your amazon s3 secret',
-    bucket: 'amazon s3 bucket name',
-    domain: 'domain bucket is served on, e.g. <bucket>.s3.amazonaws.com'
-  },
-  creationKey: 'passkey required to upload files'
-};
